ea176d412252dea5f757a8178a051ad288ed2852
Pierre Bermudez <bermudezpierre@gmail.com>
2025-12-12 18:52:30 +0100

    fix(layout): améliorer la structure du composant Navbar

--- app/routes/layout.tsx
+++ app/routes/layout.tsx
@@ -6,26 +6,22 @@ import Footer from "~/components/Footer";
 
 export default function App() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col min-h-screen w-full">
       <Navbar
         isBlurred
+        maxWidth="full"
         classNames={{
-          base: "bg-white/10 backdrop-blur-xl backdrop-saturate-150 border-slate-800/50 px-2 sm:px-4 lg:px-8 sm:h-20 fixed top-0 left-0 right-0 z-50",
+          base: "bg-white/10 backdrop-blur-xl backdrop-saturate-150 border-slate-800/50 px-2 sm:px-4 lg:px-8 sm:h-20 fixed top-0 left-0 right-0 z-50 w-full",
         }}
       >
-        <NavbarContent>
-          <NavbarBrand className="gap-4">
+        <NavbarContent justify="start">
+          <NavbarBrand className="gap-4 w-full">
             <Link to="/" className="flex items-center justify-between gap-4">
               <img src={Logo} alt="Logo" className="h-12 w-12" />
               <p className="font-bold text-inherit">BG Remover</p>
             </Link>
           </NavbarBrand>
         </NavbarContent>
-
-        <NavbarContent
-          className="hidden gap-4 sm:flex"
-          justify="center"
-        ></NavbarContent>
       </Navbar>
 
       <main className="flex-1">
